9cfc549fea87dcf436fc8450da5a534b10cfb31a
Prashant Sharma <prashant.sharma@thoughts2binary.com>
2024-09-20 19:39:45 +0530

    Added airports

--- src/seeders/20240920134141-add-airplanes.js
+++ src/seeders/20240920134141-add-airplanes.js
@@ -1,8 +1,7 @@
-'use strict';
-
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -11,16 +10,50 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-     *
-    */
+     */
+    await queryInterface.bulkInsert(
+      "Airplanes",
+      [
+        {
+          modelNumber: "Boeing 737",
+          capacity: 300,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          modelNumber: "Airbus A320",
+          capacity: 350,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          modelNumber: "Boeing 777",
+          capacity: 400,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          modelNumber: "Boeing 747",
+          capacity: 320,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          modelNumber: "Airbus A330",
+          capacity: 150,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
-
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
