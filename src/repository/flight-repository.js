f7e7adbdb9af336875d9cfb055dfaac8ce372b75
Prashant Sharma <prashant.sharma@thoughts2binary.com>
2024-09-21 20:40:05 +0530

    Added filters for flights search

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,8 +1,47 @@
 const { Flight } = require("../models/index");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 class FlightRepository {
   #createFilter(data) {
-    console.log(data);
+    let filter = {};
+    if (data.arrivalAirportId) {
+      filter.arrivalAirportId = data.arrivalAirportId;
+    }
+    if (data.departureAirportId) {
+      filter.departureAirportId = data.departureAirportId;
+    }
+    if (data.departureDate) {
+      let startingTime = new Date(data.departureDate);
+      let endingTime = new Date(startingTime.getTime() + 24 * 60 * 60 * 1000);
+      Object.assign(filter, {
+        departureTime: {
+          [Op.gte]: startingTime,
+          [Op.lte]: endingTime,
+        },
+      });
+    }
+    if (data.minPrice && data.maxPrice) {
+      Object.assign(filter, {
+        price: {
+          [Op.gte]: data.minPrice,
+          [Op.lte]: data.maxPrice,
+        },
+      });
+    }
+
+    if (data.minPrice && !data.maxPrice) {
+      Object.assign(filter, {
+        price: { [Op.gte]: data.minPrice },
+      });
+    }
+
+    if (data.minPrice && !data.maxPrice) {
+      Object.assign(filter, {
+        price: { [Op.lte]: data.maxPrice },
+      });
+    }
+    return filter;
   }
   async createFlight(data) {
     try {
@@ -27,6 +66,11 @@ class FlightRepository {
   async getAllFlights(filter) {
     try {
       let filterObject = this.#createFilter(filter);
+      console.log("filterObject", filterObject);
+      const flights = await Flight.findAll({
+        where: filterObject,
+      });
+      return flights;
     } catch (error) {
       console.log("Something went wrong in the repository layer");
       throw { error };
